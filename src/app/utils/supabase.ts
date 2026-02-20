49121f48885d1a65f830ab7fdc26c0378823c03f
Souvik Das <das.souvikk99@gmail.com>
2026-02-21 01:59:07 +0530

    Update supabase.ts

--- src/app/utils/supabase.ts
+++ src/app/utils/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://znyepubvxhkeoxecxjxd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpueWVwdWJ2eGhrZW94ZWN4anhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1ODE1NjEsImV4cCI6MjA4NzE1NzU2MX0.AbjwXi1yKlToYEQxu-w6US3aKId2Su85f_kyz5b9Akc';
+// import.meta.env is how Vite accesses your .env file
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
